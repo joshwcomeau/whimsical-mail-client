b9ee6af7f5cd6dd617bc74ca4159339ccc9fa000
Joshua Comeau <joshwcomeau@gmail.com>
2018-03-13 08:27:12 -0400

    ChildTraveller refactor continues: pre-FLIP, getting elements positioned sans animation

--- src/components/ChildTraveller/ChildTraveller.js
+++ src/components/ChildTraveller/ChildTraveller.js
@@ -20,18 +20,43 @@ type Props = {
   handleFinishJourney?: () => void,
 };
 
-type State = {};
+type State = {
+  cachedTarget?: ClientRect,
+};
 
 type Quadrant = 1 | 2 | 3 | 4;
+type Position = {
+  top?: number,
+  left?: number,
+  right?: number,
+  bottom?: number,
+};
 
 const fastSpring = { stiffness: 150, damping: 20 };
 const slowSpring = { stiffness: 150, damping: 23 };
 
 class ChildTraveller extends Component<Props, State> {
-  childWrapper: HTMLElement;
+  childWrapperNode: HTMLElement;
+  state = {};
 
   componentDidMount() {
-    this.forceUpdate();
+    this.setState({
+      cachedTarget: this.childWrapperNode.getBoundingClientRect(),
+    });
+  }
+
+  componentWillReceiveProps(nextProps: Props) {
+    this.setState({
+      cachedTarget: this.childWrapperNode.getBoundingClientRect(),
+    });
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    const hasPositionChanged = this.props.target !== prevProps.target;
+
+    if (hasPositionChanged) {
+      const newChildLocation = this.childWrapperNode.getBoundingClientRect();
+    }
   }
 
   getTargetQuadrant() {
@@ -79,7 +104,7 @@ class ChildTraveller extends Component<Props, State> {
   getTargetTranslate(quadrant: Quadrant) {
     const { target } = this.props;
 
-    const childBox = this.childWrapper.getBoundingClientRect();
+    const childBox = this.childWrapperNode.getBoundingClientRect();
 
     // If we're going "to" the target, we want to disappear into its center.
     if (this.props.direction === 'to') {
@@ -90,7 +115,6 @@ class ChildTraveller extends Component<Props, State> {
     }
 
     // Otherwise, the translate values will be based on the quadrant.
-    console.log(childBox.width);
     switch (quadrant) {
       case 1:
         return {
@@ -140,6 +164,125 @@ class ChildTraveller extends Component<Props, State> {
     }
   }
 
+  getChildPosition(quadrant: Quadrant): Position {
+    /**
+     * Get the top/left/right/bottom position for the child, relative to the
+     * current target.
+     *
+     * This depends on a few factors:
+     *
+     * 1. QUADRANT
+     *     The quadrant affects how the child will be positioned relative to the
+     *     target. In the first quadrant (top-left), the box opens from the
+     *     target's bottom-right corner:
+     *      _____
+     *     |  T  |
+     *     |_____| _____                     T = target
+     *            |  C  |                    C = child
+     *            |_____|
+     *
+     *     When we're in the second quadrant, though, the child opens to the
+     *     _left_ of the target:
+     *                                                         _____
+     *                                                        |  T  |
+     *                                                  _____  -----
+     *                                                |  C  |
+     *                                                 -----
+     *     Effectively, each quadrant causes the child to open from the target's
+     *     _opposite corner_. This is to ensure that the child opens on-screen
+     *     (if it always opened to the top-right, and the target was also in
+     *     the top-right corner, it would render outside of the viewport).
+     *
+     * 2. DIRECTION
+     *     When the direction is `from` the target, we want to open from its
+     *     opposite corner (as shown in 1. QUADRANT).
+     *     When the direction is `to`, though, we want to align the target's
+     *     center-point to the child's center-point:
+     *
+     *     `from`:
+     *      _______
+     *     |       |
+     *     |   T   |
+     *     |       |                        T = target
+     *      ------- ___                     C = child
+     *             | C |
+     *              ---
+     *
+     *     `to`:
+     *      _______
+     *     |  ___  |
+     *     | | C | |
+     *     |  ---  |
+     *      -------
+     *
+     *     The reason for this is that when travelling TO an element, we're also
+     *     shrinking it. We want it to disappear into the target, and so we
+     *     position it right in the center.
+     */
+    const { target, direction, windowWidth, windowHeight } = this.props;
+
+    const childBox = this.childWrapperNode.getBoundingClientRect();
+
+    const childBoxCenter = {
+      x: childBox.left + childBox.width / 2,
+      y: childBox.top + childBox.height / 2,
+    };
+
+    const targetCenter = {
+      x: target.left + target.width / 2,
+      y: target.top + target.height / 2,
+    };
+
+    switch (quadrant) {
+      case 1:
+        return {
+          top:
+            direction === 'from'
+              ? target.bottom
+              : targetCenter.y - childBox.height / 2,
+          left:
+            direction === 'from'
+              ? target.right
+              : targetCenter.x - childBox.width / 2,
+        };
+      case 2:
+        return {
+          top:
+            direction === 'from'
+              ? target.bottom
+              : targetCenter.y - childBox.height / 2,
+          right:
+            direction === 'from'
+              ? windowWidth - target.left
+              : windowWidth - targetCenter.x - childBox.width / 2,
+        };
+      case 3:
+        return {
+          bottom:
+            direction === 'from'
+              ? windowHeight - target.top
+              : windowHeight - targetCenter.y - childBox.height / 2,
+          left:
+            direction === 'from'
+              ? target.right
+              : targetCenter.x - childBox.width / 2,
+        };
+      case 4:
+        return {
+          bottom:
+            direction === 'from'
+              ? windowHeight - target.top
+              : windowHeight - targetCenter.y - childBox.height / 2,
+          right:
+            direction === 'from'
+              ? windowWidth - target.left
+              : windowWidth - targetCenter.x - childBox.width / 2,
+        };
+      default:
+        throw new Error(`Unrecognized quadrant: ${quadrant}`);
+    }
+  }
+
   render() {
     const { target, direction, children } = this.props;
 
@@ -148,14 +291,15 @@ class ChildTraveller extends Component<Props, State> {
     const transformOrigin = this.getTargetTransformOrigin(quadrant);
 
     let targetValues;
-    if (this.childWrapper) {
-      const { translateX, translateY } = this.getTargetTranslate(quadrant);
+    if (this.childWrapperNode) {
+      // const { translateX, translateY } = this.getTargetTranslate(quadrant);
 
       targetValues = {
         scaleX: spring(direction === 'from' ? 1 : 0),
         scaleY: spring(direction === 'from' ? 1 : 0),
-        translateX: direction === 'from' ? translateX : spring(translateX),
-        translateY: direction === 'from' ? translateY : spring(translateY),
+        ...this.getChildPosition(quadrant),
+        // translateX: direction === 'from' ? translateX : spring(translateX),
+        // translateY: direction === 'from' ? translateY : spring(translateY),
       };
     } else {
       targetValues = {
@@ -164,33 +308,44 @@ class ChildTraveller extends Component<Props, State> {
       };
     }
 
+    console.log(targetValues);
+
     return (
       <Motion
         defaultStyle={{
           scaleX: 1,
           scaleY: 1,
-          translateX: target.left,
-          translateY: target.top,
+          // translateX: target.left,
+          // translateY: target.top,
         }}
         style={targetValues}
       >
-        {({ scaleX, scaleY, translateX, translateY }) => (
-          <OuterWrapper
-            innerRef={node => (this.childWrapper = node)}
-            style={{ display: 'inline-block' }}
+        {({
+          scaleX,
+          scaleY,
+          translateX,
+          translateY,
+          top,
+          left,
+          bottom,
+          right,
+        }) => (
+          <Wrapper
+            innerRef={node => (this.childWrapperNode = node)}
+            style={{
+              top,
+              left,
+              bottom,
+              right,
+              // transform: `
+              //       translate(${translateX}px, ${translateY}px)
+              //       scale(${scaleX}, ${scaleY})
+              //     `,
+              transformOrigin,
+            }}
           >
-            <Wrapper
-              style={{
-                transform: `
-                    translate(${translateX}px, ${translateY}px)
-                    scale(${scaleX}, ${scaleY})
-                  `,
-                transformOrigin,
-              }}
-            >
-              {children}
-            </Wrapper>
-          </OuterWrapper>
+            {children}
+          </Wrapper>
         )}
       </Motion>
     );
@@ -204,6 +359,8 @@ const OuterWrapper = styled.div`
   left: 0;
 `;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  position: absolute;
+`;
 
 export default ChildTraveller;
